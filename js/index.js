bd40cd52e80408155ddb898f541349b0ef760385
kame <midorima570@gmail.com>
2021-08-26 17:48:42 +0900

    display->visibility

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 $(function () {
-    $(document).on('click','.ac-parent', function () {
-    $(this).next().slideToggle();
-    if($(this).children().next().text()=="expand_more"){
-        $(this).children().next().text("expand_less");
-    }else{
-        $(this).children().next().text("expand_more");
-    }
-  });
-
+    $('.ac-parent').click( function () {
+        $(this).next().slideToggle();
+        if($(this).children().next().text()=="expand_more"){
+            $(this).children().next().text("expand_less");
+        }else{
+            $(this).children().next().text("expand_more");
+        }
+      }
+    );
+   
     $('.hamburger').click(function(){
         $('.hamburger_menu').toggleClass('open');
         $('.hamburger').children('span').toggleClass('hamburger_white');
+        
     });
 
 });
