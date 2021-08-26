f000f74d38592e9ad17f8aa7d971ba3fc34c80b4
kame <midorima570@gmail.com>
2021-08-26 17:33:31 +0900

    スマホ対応

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 $(function () {
-    $('.ac-parent').on('click', function () {
-    $(this).next().slideToggle();
-    if($(this).children().next().text()=="expand_more"){
-        $(this).children().next().text("expand_less");
-    }else{
-        $(this).children().next().text("expand_more");
-    }
-  });
+    $('.ac-parent').click(function () {
+        $(this).next().slideToggle();
+        if($(this).children().next().text()=="expand_more"){
+            $(this).children().next().text("expand_less");
+        }else{
+            $(this).children().next().text("expand_more");
+        }
+      });
 
     $('.hamburger').click(function(){
         $('.hamburger_menu').toggleClass('open');
